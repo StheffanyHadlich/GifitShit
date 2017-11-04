b5781ec039a6ea3544946a2bcdc8f519adba9adf
Stheffany Hadlich <tefani.hadlich@gmail.com>
2017-11-04 14:30:01 -0200

    Server add

---
+++ Server/Data/AcademicContext.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+using Server.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Server.Data
+{
+    public class ServerContext : DbContext
+    {
+        public DbSet<Client> Client { get; set; }
+        public DbSet<Product> Product { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=./Data/database.sqlite");
+        }
+
+        public static void InitDb(IServiceProvider serviceProvider)
+        {
+            const string path = "./Data/database.sqlite";
+            if (File.Exists(path))
+            {   
+                File.Delete(path);
+            }
+            var serviceScope = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope();
+            var dataContext = serviceScope.ServiceProvider.GetRequiredService<ServerContext>();
+            dataContext.Database.EnsureCreated();
+            dataContext.SaveChanges();
+        }
+    }
+}
---
+++ Server/Models/Product.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Server.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; } = "";
+        public string Description { get; set; } = "";
+        public float Price {get; set; } = 0;
+        public Client Seller {get; set; }        
+        public string Image { get; set; }=" ";
+
+       
+
+    }
+}
